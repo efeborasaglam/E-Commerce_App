7c6ccb882d62cf2c2468d63095432da36c4875cc
efeborasaglam <EfeBora.Saglam@hotmail.com>
2026-08-12 15:15:45 +0200

    admin successfully added

---
+++ admin/src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useContext } from "react";
+import Login from "./pages/Login.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import { AdminContext } from "./context/AdminContext.jsx";
+import NavBar from "./components/NavBar.jsx";
+import SideBar from "./components/SideBar.jsx";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard.jsx";
+import AllApointments from "./pages/Admin/AllApointments.jsx";
+import AddDoctor from "./pages/Admin/AddDoctor.jsx";
+import DoctorsList from "./pages/Admin/DoctorsList.jsx";
+import { DoctorContext } from "./context/DoctorContext.jsx";
+import DoctorDashboard from "./pages/Doctor/DoctorDashboard.jsx";
+import DoctorAppointment from "./pages/Doctor/DoctorAppointment.jsx";
+import DoctorProfile from "./pages/Doctor/DoctorProfile.jsx";
+
+const App = () => {
+  const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
+
+  return aToken || dToken ? (
+    <div className={"bg-[#F8F9FD]"}>
+      <ToastContainer />
+      <NavBar />
+      <div className={"flex items-start"}>
+        <SideBar />
+        <Routes>
+          {/* Admin Route */}
+          <Route path={"/"} element={<></>} />
+          <Route path={"/admin-dashboard"} element={<Dashboard />} />
+          <Route path={"/all-appointments"} element={<AllApointments />} />
+          <Route path={"/add-doctor"} element={<AddDoctor />} />
+          <Route path={"/doctor-list"} element={<DoctorsList />} />
+          {/* Doc Route */}
+          <Route path={"/doctor-dashboard"} element={<DoctorDashboard />} />
+          <Route
+            path={"/doctor-appointments"}
+            element={<DoctorAppointment />}
+          />
+          <Route path={"/doctor-profile"} element={<DoctorProfile />} />
+        </Routes>
+      </div>
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  );
+};
+export default App;
---
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -0,0 +1,184 @@
+import React, { useContext, useEffect, useState } from "react";
+import { DoctorContext } from "../../context/DoctorContext.jsx";
+import { AppContext } from "../../context/AppContext.jsx";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const DoctorProfile = () => {
+  const { dToken, profileData, setProfileData, getProfileData, backendUrl } =
+    useContext(DoctorContext);
+  const { currency } = useContext(AppContext);
+
+  const [isEdit, setIsEdit] = useState(false);
+
+  const updateProfile = async () => {
+    try {
+      const updateData = {
+        address: profileData.address,
+        fees: profileData.fees,
+        available: profileData.available,
+      };
+
+      const { data } = await axios.post(
+        backendUrl + "/api/doctor/update-profile",
+        updateData,
+        { headers: { dToken } },
+      );
+      if (data.success) {
+        toast.success(data.message);
+        setIsEdit(false);
+        getProfileData();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (e) {
+      toast.error(e.message);
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (dToken) {
+      getProfileData();
+    }
+  }, [dToken]);
+
+  return (
+    profileData && (
+      <div className={"flex flex-col gap-4 m-5"}>
+        <div>
+          <div>
+            <img
+              className={"bg-primary/80 w-full sm:max-w-64 rounded-lg"}
+              src={profileData.image}
+              alt={"doc image"}
+            />
+          </div>
+          <div
+            className={
+              "flex-1 border border-stone-100 rounded-lg p-8 py-7 bg-white"
+            }
+          >
+            {/*name. degree, experience --- doc info*/}
+            <p
+              className={
+                "flex items-center gap-2 text-3xl font-medium text-gray-700"
+              }
+            >
+              {profileData.name}
+            </p>
+            <div className={"flex items-center gap-2 mt-1 text-gray-600"}>
+              <p>
+                {profileData.degree} - {profileData.speciality}
+              </p>
+              <button className={"py-0.5 px-2 border text-xs rounded-full"}>
+                {profileData.experience}
+              </button>
+            </div>
+            {/*  doc about */}
+            <div>
+              <p
+                className={
+                  "flex items-center gap-1 text-sm font-medium text-neutral-800 mt-3"
+                }
+              >
+                About:{" "}
+              </p>
+              <p className={"text-sm text-gray-600 max-w-[700px] mt-1"}>
+                {profileData.about}
+              </p>
+            </div>
+            <p className={"text-gray-600 font-medium mt-4"}>
+              Appointment fee:{" "}
+              <span className={"text-gray-800"}>
+                {currency}{" "}
+                {isEdit ? (
+                  <input
+                    value={profileData.fees}
+                    type={"number"}
+                    onChange={(e) =>
+                      setProfileData((prev) => ({
+                        ...prev,
+                        fees: e.target.value,
+                      }))
+                    }
+                  />
+                ) : (
+                  profileData.fees
+                )}
+              </span>
+            </p>
+            <div className={"flex gap-2 py-2"}>
+              <p>Address: </p>
+              <p className={"text-sm"}>
+                {isEdit ? (
+                  <input
+                    type={"text"}
+                    value={profileData.address.line1}
+                    onChange={(e) =>
+                      setProfileData((prev) => ({
+                        ...prev,
+                        address: { ...prev.address, line1: e.target.value },
+                      }))
+                    }
+                  />
+                ) : (
+                  profileData.address.line1
+                )}
+                <br />{" "}
+                {isEdit ? (
+                  <input
+                    type={"text"}
+                    value={profileData.address.line2}
+                    onChange={(e) =>
+                      setProfileData((prev) => ({
+                        ...prev,
+                        address: { ...prev.address, line2: e.target.value },
+                      }))
+                    }
+                  />
+                ) : (
+                  profileData.address.line2
+                )}
+              </p>
+            </div>
+            <div className={"flex gap-1 pt-2"}>
+              <input
+                checked={profileData.available}
+                onChange={() =>
+                  isEdit &&
+                  setProfileData((prev) => ({
+                    ...prev,
+                    available: !prev.available,
+                  }))
+                }
+                type={"checkbox"}
+              />
+              <label htmlFor={""}>Available</label>
+            </div>
+            {isEdit ? (
+              <button
+                onClick={updateProfile}
+                className={
+                  "px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all"
+                }
+              >
+                Save
+              </button>
+            ) : (
+              <button
+                onClick={() => setIsEdit(true)}
+                className={
+                  "px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all"
+                }
+              >
+                Edit
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+export default DoctorProfile;
