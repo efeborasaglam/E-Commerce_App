ff04c1605590a5d59f74dfafeefcb49606552f22
efeborasaglam <efebora.saglam@hotmail.com>
2026-09-15 16:17:19 +0200

    release

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/api/user', userRouter)
 
 app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
-app.get('*any', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 })
 
